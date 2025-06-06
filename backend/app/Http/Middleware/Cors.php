6bd5def5793aee787743699645e5b2e291eb5ff3
BenitoFB88 <150259817+BenitoFB88@users.noreply.github.com>
2025-06-05 22:05:08 -0400

    se crea funcion que llama n a mas folio

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -8,8 +8,16 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
+        // Si es una petición OPTIONS, responde inmediatamente
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json('OK', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
         $response = $next($request);
-        
+
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
